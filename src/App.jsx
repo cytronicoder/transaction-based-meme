e85d4777ec8ef6c200f40b17f708b8134f934a13
Zeyu Yao <novodoodle@gmail.com>
2023-01-03 19:31:14 +0800

    Implement button that connects Metamask account

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,81 @@
-import './App.css'
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 export default function App() {
-  return (
-    <main>
-      React ⚛️ + Vite ⚡ + Replit 🌀
-    </main>
-  )
+    const [currentAccount, setCurrentAccount] = useState(null);
+
+    /**
+     * This function does a few things:
+     * 1. It checks to see if Metamask is installed
+     * 2. If it is, it checks to see if the user has connected their wallet previously
+     * 3. If they have, it grabs their public wallet address and sets it in our state
+     * 4. If they haven't, it asks them to connect their wallet to our dApp
+     */
+    const checkIfWalletIsConnected = () => {
+        // First make sure we have access to window.ethereum (Metamask)
+        const { ethereum } = window;
+        if (!ethereum) {
+            console.log("🦊 Make sure you have Metamask installed!");
+            return;
+        } else {
+            console.log("💸 Ethereum object exists!");
+        }
+
+        // Check if we're authorized to access the user's wallet
+        ethereum.request({ method: "eth_accounts" }).then((accounts) => {
+            if (accounts.length !== 0) {
+                const account = accounts[0];
+                console.log("👛", account);
+                setCurrentAccount(account);
+            } else {
+                console.log(
+                    "🤔 No authorized account found. You need to connect your Metamask wallet."
+                );
+            }
+        });
+    };
+
+    /**
+     * This function connects the user's wallet to our dApp
+     */
+    const connectWallet = () => {
+        // Metamask access
+        const { ethereum } = window;
+        if (!ethereum) {
+            alert("Make sure you have Metamask installed! 🦊");
+        }
+
+        // Request account access if needed
+        ethereum
+            .request({ method: "eth_requestAccounts" })
+            .then((accounts) => {
+                console.log("Connected", accounts[0]);
+                setCurrentAccount(accounts[0]);
+            })
+            .catch((err) => console.log(err));
+    };
+
+    // Whenever the page loads, check to see if the user has connected their wallet
+    useEffect(() => {
+        checkIfWalletIsConnected();
+    }, []);
+
+    return (
+        <main>
+            <div className="container">
+                {/* Header + description */}
+                <div className="header">Transaction-based Memes</div>
+                <div className="description">
+                    Input a transaction hash to see the meme associated with it.
+                </div>
+
+                {/* If the user has not connected their wallet, display the connect wallet button */}
+                {!currentAccount && (
+                    <button className="connectButton" onClick={connectWallet}>
+                        Connect Wallet
+                    </button>
+                )}
+            </div>
+        </main>
+    );
 }
