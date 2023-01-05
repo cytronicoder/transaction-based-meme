b8d92979b701cb20ee969b23782be2f3fc81a1f6
cytronicoder <novodoodle@gmail.com>
2023-01-05 15:43:15 +0800

    Add transaction history table

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 export default function App() {
     const [loggedAddress, setLoggedAddress] = useState(null);
     const [selectedAddress, setSelectedAddress] = useState("");
+    const [etherscanData, setEtherscanData] = useState(null);
 
     /**
      * This is a secret key that allows us to access Etherscan's API.
@@ -70,10 +71,17 @@ export default function App() {
 
     /**
      * This function searches for a transaction hash in Etherscan
-     * TODO: Implement this function
+     * and sets the meme based on the result
      */
     const searchTransaction = () => {
-        console.log("Searching for transaction hash:", selectedAddress);
+        console.log("Searching for transaction hash: ", selectedAddress);
+
+        // Returns the list of transactions performed by an address
+        const url = `https://api-sepolia.etherscan.io/api?module=account&action=txlist&address=${selectedAddress}&startblock=0&endblock=99999999&sort=asc&apikey=${etherscan_api_key}`;
+        const data = fetch(url).then((res) => res.json());
+        data.then((data) => {
+            setEtherscanData(data);
+        });
     };
 
     // Whenever the page loads, check to see if the user has connected their wallet
@@ -95,35 +103,72 @@ export default function App() {
                   Otherwise, display their wallet address.
                 */}
                 {loggedAddress ? (
-                    <div className="account">
-                        {loggedAddress.slice(0, 6)}...
-                        {loggedAddress.slice(-4)}
-                        {/* {loggedAddress} */}
-                    </div>
+                    <>
+                        <div className="account">
+                            {loggedAddress.slice(0, 6)}...
+                            {loggedAddress.slice(-4)}
+                            {/* {loggedAddress} */}
+                        </div>
+
+                        {/* Input field for transaction hash once wallet is connected */}
+                        <div className="inputContainer">
+                            <input
+                                className="input"
+                                type="text"
+                                placeholder="Enter transaction hash"
+                                onChange={onInputChange}
+                            />
+                            <button
+                                className="submitButton"
+                                onClick={searchTransaction}
+                            >
+                                Search
+                            </button>
+                        </div>
+
+                        {/* Display transactions in a table */}
+                        {etherscanData && (
+                            <div className="tableContainer">
+                                <table className="table">
+                                    <thead>
+                                        <tr>
+                                            <th>Block Number</th>
+                                            <th>From</th>
+                                            <th>To</th>
+                                            <th>Value</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {/* get the first 5 */}
+                                        {etherscanData.result.slice(0, 5).map((tx) => (
+                                            <tr key={tx.hash}>
+                                                <td>{tx.blockNumber}</td>
+                                                <td>
+                                                    {tx.from.slice(0, 6)}...
+                                                    {tx.from.slice(-4)}
+                                                </td>
+                                                <td>
+                                                    {tx.to.slice(0, 6)}...
+                                                    {tx.to.slice(-4)}
+                                                </td>
+                                                <td>
+                                                    {parseFloat(
+                                                        tx.value / 1000000000000000000
+                                                    ).toFixed(4)}
+                                                </td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
+                        )}
+
+                        {/* Display memes based on transaction hash */}
+                    </>
                 ) : (
                     <button className="button" onClick={connectWallet}>
                         Connect Wallet
                     </button>
-                )}
-
-                {/* Input field for transaction hash once wallet is connected 
-                TODO: Add CSS to make this look better
-                */}
-                {loggedAddress && (
-                    <div className="inputContainer">
-                        <input
-                            className="input"
-                            type="text"
-                            placeholder="Enter transaction hash"
-                            onChange={onInputChange}
-                        />
-                        <button
-                            className="submitButton"
-                            onClick={searchTransaction}
-                        >
-                            Search
-                        </button>
-                    </div>
                 )}
             </div>
         </main>
